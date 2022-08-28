1eb018962a56c0eac06977bb1241caa9dc474ce4
LorenaBustosJ <lorenabustosjasson@gmail.com>
2022-08-28 18:48:45 -0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { HsComponent } from './components/hs/hs.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     EducacionComponent,
     HsComponent,
     ProyectoComponent,
+    FooterComponent,
 
   ],
   imports: [
