dca64bc937524c7bd95012491596a849b9b8a96d
LorenaBustosJ <lorenabustosjasson@gmail.com>
2022-10-30 18:04:30 -0300

    .

---
+++ BackEnd/src/main/java/org/springframework/security/core/SimpleGarantedAuthority.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.springframework.security.core;
+
+/**
+ *
+ * @author Lorena Bustos
+ */
+public class SimpleGarantedAuthority {
+
+    public SimpleGarantedAuthority(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
